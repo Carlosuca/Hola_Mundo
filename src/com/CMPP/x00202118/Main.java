186f212f69ca9f48eb951ca02c47eec6886bb717
Carlosuca <00202118@uca.edu.sv>
2020-04-16 23:05:07 -0600

    prueba 1000

--- src/com/CMPP/x00202118/Main.java
+++ src/com/CMPP/x00202118/Main.java
@@ -3,6 +3,6 @@ package com.CMPP.x00202118;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo" + "haciendo prubas de commits porque no seccscfss suben" );
+        System.out.println("Hola Mundo" + "haciendo prubas de commits a la bestia alv  suben" );
     }
 }
